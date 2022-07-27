eb64ec71c7be1eb1e6d3904b56e93972689c2104
DG <gdavidcefim@gmail.com>
2022-07-27 11:46:12 +0200

    Style activity chart

--- src/components/dashboard/activity/index.jsx
+++ src/components/dashboard/activity/index.jsx
@@ -2,6 +2,7 @@ import { API_PATH } from "../../../constants"
 import { useFetch } from "../../../services/http"
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './index.css'
+import ActivityModel from "../../../models/Activity";
 
 const Activity = () => {
   const { data: activity, isLoading: activityIsLoading, error: activityError} = useFetch(API_PATH.ACTIVITY);
@@ -12,29 +13,31 @@ const Activity = () => {
   if(activityError){
     return <p>error</p>
   }
-  const userActivity = activity.filter(user => user.userId === 12)[0].sessions;
-  
+  const userActivity = new ActivityModel(activity.filter(user => user.userId === 12)[0]);
+  console.log('model', userActivity.formattedSessions)
   return (
     <section className="activity">
+      <h2>Activité quotidienne</h2>
       <ResponsiveContainer width="100%" height="100%">
         <BarChart
           width={500}
           height={10}
-          data={userActivity}
+          data={userActivity.formattedSessions}
           margin={{
-            top: 5,
+            top: 32,
             right: 30,
             left: 20,
             bottom: 5,
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis  domain={[1,100]}/>
-          <YAxis />
+          <XAxis dataKey="day"/>
+          <YAxis yAxisId="kilogram" dataKey="kilogram" domain={[dataMax => dataMax - 2, dataMax => dataMax + 1]} orientation={"right"} tickCount={3} />
+          <YAxis yAxisId="calories" dataKey="calories" hide={true} />
           <Tooltip />
-          <Legend />
-          <Bar className="kilogramBar" dataKey="kilogram" radius={[100, 100, 0, 0,]} barSize={7}/>
-          <Bar className="caloriesBar" dataKey="calories" radius={[100, 100, 0, 0,]} barSize={7}/>
+          <Legend verticalAlign="top" align="right" iconType="circle" height={72}/>
+          <Bar yAxisId="kilogram" className="kilogramBar" dataKey="kilogram" radius={[100, 100, 0, 0,]} barSize={7} />
+          <Bar yAxisId="calories" className="caloriesBar" dataKey="calories" radius={[100, 100, 0, 0,]} barSize={7}/>
         </BarChart>
       </ResponsiveContainer>
     </section>
--- src/models/Activity.js
+++ src/models/Activity.js
@@ -2,6 +2,19 @@ class ActivityModel {
   constructor(data){
     this.sessions = data.sessions
   }
+
+  formattedDate (date) {
+    return new Date(date).getDate();
+  }
+
+  get formattedSessions () {
+    return this.sessions.map(session => (
+      { day: this.formattedDate(session.day), 
+        kilogram: session.kilogram,
+        calories: session.calories
+      })
+    )
+  }
 }
 
 export default ActivityModel
